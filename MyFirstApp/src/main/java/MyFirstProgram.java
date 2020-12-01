9909301c506d0661d44c82dd85ee771e48c0c2a4
Nataliia-Pishniak <--global>
2020-12-01 14:41:30 +0200

    Initial commit

---
+++ MyFirstApp/src/main/java/MyFirstProgram.java
@@ -0,0 +1,41 @@
+public class MyFirstProgram {
+
+    public static void main(String[] args) {
+        int x;
+        x = 5;
+        int a, b, c;
+        final int z = 5;
+
+
+        boolean boolT = true, boolF = false;
+
+        //byte -128 to 127and w 1 byte.
+        byte bt = 100;
+
+        //-32768 to 32767;
+        short shrt = 32767;
+
+        //-2147483648 to 2147483647
+        int intenger = 2147483647;
+
+        //-9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+        long long1 = 9223372036854775807L;
+
+        //
+        double dbl = 8.5;
+        float flt = 2.6F;
+
+        char chr = 'c';
+        char one = 223;
+
+        String file = "text \n text1";
+        System.out.println(file);
+
+        String file2 = "Hello December\nHello Winter 2020";
+        System.out.println(file2);
+
+
+    }
+}
+
+
---
+++ MyFirstApp/src/main/java/SB_HW.java
@@ -0,0 +1,38 @@
+public class SB_HW {
+
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder ("Hello Winter 2020\n");
+
+        System.out.println(sb);
+
+        sb.append("Yaay");
+        System.out.println(sb);
+
+        sb.delete(0,2);
+        System.out.println(sb);
+
+        sb.deleteCharAt(7);
+        System.out.println(sb);
+
+        sb.insert( 0, 'H');
+        System.out.println(sb);
+
+        sb.insert( 8, 't');
+        System.out.println(sb);
+        sb.replace(1,1, "eee" );
+
+        System.out.println(sb);
+
+        sb.reverse();
+        System.out.println(sb);
+
+        sb.reverse();
+
+        System.out.println("substring is = "+sb.substring(8));
+        System.out.println("substring is = "+sb.substring(8,14));
+
+    }
+
+
+}
