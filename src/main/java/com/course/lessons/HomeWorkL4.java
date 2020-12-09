5249383e3d0cef3dc25ce2bd12ff7bf2ec36709d
Nataliia-Pishniak <npishnyak123@gmail.com>
2020-12-09 15:03:04 +0200

    new hw L4

--- src/main/java/com/course/lessons/HomeWorkL4.java
+++ src/main/java/com/course/lessons/HomeWorkL4.java
@@ -7,31 +7,35 @@ public class HomeWorkL4 {
         //С помощью цикла foreach вывести в консоль цифри больше 0:
         int[] nums = {-1, 2, 0, -5, -7, 6};
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > 0) {
-                System.out.println(nums[i]);}
+        for (int i : nums) {
+            if (i > 0) {
+                System.out.println(i);
             }
+        }
         //С помощью цикла for со счетчиком вывести в консоль цифри меньше 0:
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] < 0) {
-                System.out.println(nums[i]);}
+                System.out.println(nums[i]);
+            }
         }
         //С помощью цикла for со счетчиком заменить отрицательные числа в масиве на ноль
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] < 0) {
-                System.out.println(0);}
-             else {
-                System.out.println(nums[i]);
-             }
+                nums[i] = 0;
+            }
+            System.out.println( "array without negative values: "+ nums[i]);
+        }
 
         //С помощью цикла for со счетчиком вывести в консоль сумму всех цифер
-            int sum =0;
-            for (i = 0; i < nums.length; i++) {
-            sum = sum+nums [i];
-            }
-            System.out.println("sum: " + sum); }
-
-
-
+        int sum = 0;
+        int i;
+        for (i = 0; i < nums.length; i++) {
+            sum = sum + nums[i];
         }
+        System.out.println("sum: " + sum);
     }
+
+
+}
+
+
